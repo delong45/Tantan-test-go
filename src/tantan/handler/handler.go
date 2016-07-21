8ddf3a4935a84a87164db96c40322c31bfece0c4
delong1 <delong1@staff.weibo.com>
2016-07-21 19:06:55 +0800

    relationships complete

--- src/tantan/handler/handler.go
+++ src/tantan/handler/handler.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"tantan/user"
 	"tantan/utils"
+
+	"github.com/gorilla/mux"
 )
 
 type UserResp struct {
@@ -94,8 +96,69 @@ func UserProcess(w http.ResponseWriter, r *http.Request) {
 }
 
 func RelationList(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprintf(w, "Relationships do not support such method\n")
+		return
+	}
+
+	vars := mux.Vars(r)
+	user_id := vars["user_id"]
+	rs, err := user.GetRelations(user_id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Server internal error: %v\n", err)
+		return
+	}
+
+	response, err := json.Marshal(rs)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Server internal error: %v\n", err)
+		return
+	}
+	fmt.Fprintf(w, "%s", response)
 }
 
 func RelationCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Relation Create!\n"))
+	if r.Method != "PUT" {
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprintf(w, "Relation Creator do not support such method\n")
+		return
+	}
+
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Server internal error: %v\n", err)
+		return
+	}
+
+	var s user.State
+	err = json.Unmarshal(body, &s)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Server internal error: %v\n", err)
+		return
+	}
+
+	vars := mux.Vars(r)
+	user_id := vars["user_id"]
+	other_user_id := vars["other_user_id"]
+
+	relation, err := user.GreateRelation(user_id, other_user_id, s.State)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Server internal error: %v\n", err)
+		return
+	}
+
+	response, err := json.Marshal(relation)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Server internal error: %v\n", err)
+		return
+	}
+	fmt.Fprintf(w, "%s", response)
 }
--- src/tantan/user/user.go
+++ src/tantan/user/user.go
@@ -12,12 +12,28 @@ type User struct {
 	Type string
 }
 
+type Relation struct {
+	User_id string `json:"user_id"`
+	State   string `json:"state"`
+	Type    string `json:"type"`
+}
+
+type Relationships struct {
+	Id       string
+	Other_id string
+	State    string
+}
+
+type State struct {
+	State string `json:"state"`
+}
+
 var db *pg.DB
 
 func createSchema() error {
 	queries := []string{
-		`CREATE TABLE users (id serial primary key, name text, type text)`,
-		`CREATE TABLE relationships (id serial primary key, liked integer[], matched integer[], disliked integer[])`,
+		`CREATE TABLE users (id serial primary key, name text, "type" text)`,
+		`CREATE TABLE relationships (id text, other_id text, state text)`,
 	}
 	for _, q := range queries {
 		_, err := db.Exec(q)
@@ -58,4 +74,83 @@ func GetUsers() ([]User, error) {
 	var users []User
 	_, err := db.Query(&users, `SELECT * FROM users`)
 	return users, err
+}
+
+func GetRelations(id string) ([]Relation, error) {
+	var relations []Relation
+	var relationships []Relationships
+	_, err := db.Query(&relationships, `SELECT * FROM relationships WHERE id = ?`, id)
+	if err != nil {
+		return relations, err
+	}
+
+	for _, r := range relationships {
+		rel := Relation{
+			User_id: r.Other_id,
+			State:   r.State,
+			Type:    "relationship",
+		}
+		relations = append(relations, rel)
+	}
+	return relations, err
+}
+
+func GreateRelation(id, other_id, state string) (Relation, error) {
+	r := Relation{
+		User_id: other_id,
+		State:   state,
+		Type:    "relationship",
+	}
+
+	if state != "liked" && state != "disliked" {
+		return r, fmt.Errorf("no such relation state")
+	}
+
+	relationships := Relationships{
+		Id:       id,
+		Other_id: other_id,
+		State:    state,
+	}
+
+	var preRelation Relationships
+	err := db.Model(&preRelation).Where("id = ?", id).Where("other_id = ?", other_id).Select()
+	if err != nil {
+		return r, err
+	}
+	if preRelation.State == state {
+		return r, nil
+	}
+	if preRelation.State == "" {
+		db.Create(&relationships)
+	}
+
+	var corRelation Relationships
+	err = db.Model(&corRelation).Where("id = ?", other_id).Where("other_id = ?", id).Select()
+	if err != nil {
+		return r, err
+	}
+	corState := corRelation.State
+
+	switch state {
+	case "liked":
+		if corState == "matched" {
+			r.State = "matched"
+		} else if corState == "liked" {
+			r.State = "matched"
+			relationships.State = "matched"
+			corRelation.State = "matched"
+			db.Model(&relationships).Column("state").Where("id = ?", id).Where("other_id = ?", other_id).Update()
+			db.Model(&corRelation).Column("state").Where("id = ?", other_id).Where("other_id = ?", id).Update()
+		} else {
+			db.Model(&relationships).Column("state").Where("id = ?", id).Where("other_id = ?", other_id).Update()
+		}
+	case "disliked":
+		if corState == "matched" {
+			corRelation.State = "liked"
+			db.Model(&corRelation).Column("state").Where("id = ?", other_id).Where("other_id = ?", id).Update()
+		}
+		db.Model(&relationships).Column("state").Where("id = ?", id).Where("other_id = ?", other_id).Update()
+	}
+
+	return r, nil
 }
